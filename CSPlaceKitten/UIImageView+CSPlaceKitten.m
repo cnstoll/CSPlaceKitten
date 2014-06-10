b202174c75b629edcc29977a62e287d6136c6b72
Conrad Stoll <conrad.stoll@mutualmobile.com>
2014-06-10 12:15:16 -0500

    Silence warning

--- CSPlaceKitten/UIImageView+CSPlaceKitten.m
+++ CSPlaceKitten/UIImageView+CSPlaceKitten.m
@@ -44,7 +44,7 @@
         height = height * screen.scale;
     }
     
-    NSString *kittenSize = [NSString stringWithFormat:@"%d/%d", width, height];
+    NSString *kittenSize = [NSString stringWithFormat:@"%ld/%ld", width, height];
     NSString *kittenAddress = [NSString stringWithFormat:@"http://placekitten.com/g/%@", kittenSize];
     NSURL *url = [NSURL URLWithString:kittenAddress];
     [self setImageWithURL:url];
